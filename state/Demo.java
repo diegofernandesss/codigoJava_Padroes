d93a0926d3ca97675d6c760ef419e874ec90ceab
Diego Fernandes <88402851+diegofernandesss@users.noreply.github.com>
2023-05-11 16:53:57 -0300

    Add files via upload

---
+++ state/Demo.java
@@ -0,0 +1,15 @@
+package state;
+
+import state.ui.Player;
+import state.ui.UI;
+
+/**
+ * Demo class. Everything comes together here.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        Player player = new Player();
+        UI ui = new UI(player);
+        ui.init();
+    }
+}
